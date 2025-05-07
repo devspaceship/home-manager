2ae9abca55e5c036f26d8473a4ced0edd98c4cda
Thomas Saint-Gérand <thomas@devspaceship.com>
2025-05-07 13:28:48 +0200

    macbook pro adaptations

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- LazyVim Extras
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- Custom
     { import = "plugins" },
   },
