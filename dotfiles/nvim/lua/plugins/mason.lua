9ba2245df47eef509830c7ab66518beb81de798e
Thomas Saint-Gérand <thomas@devspaceship.com>
2025-08-21 22:26:24 +0200

    [nvim] mason config

---
+++ dotfiles/nvim/lua/plugins/mason.lua
@@ -0,0 +1,83 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      -- Arduino
+      "arduino-language-server",
+      -- Bash
+      "bash-debug-adapter",
+      "bash-language-server",
+      "shellcheck",
+      "shfmt",
+      -- CSS
+      "css-lsp",
+      "tailwindcss-language-server",
+      -- Docker
+      "docker-language-server",
+      "hadolint",
+      -- GitHub
+      "gh-actions-language-server",
+      -- Go
+      "go-debug-adapter",
+      "gofumpt",
+      "goimports",
+      "gopls",
+      -- GraphQL
+      "graphql-language-service-cli",
+      -- HTML
+      "html-lsp",
+      -- JSON
+      "json-lsp",
+      -- Lua
+      "lua-language-server",
+      "luacheck",
+      "stylua",
+      -- Makefile
+      "autotools-language-server",
+      -- Markdown
+      "markdown-toc",
+      "markdownlint",
+      "marksman",
+      -- Nginx
+      "nginx-config-formatter",
+      "nginx-language-server",
+      -- Nix
+      "nil",
+      -- Python
+      "black",
+      "debugpy",
+      "isort",
+      "pyright",
+      "ruff",
+      -- Rust
+      "codelldb",
+      "rust-analyzer",
+      -- Security
+      "snyk",
+      -- SQL
+      "sqlfluff",
+      "sqlfmt",
+      "sqls",
+      -- Swift
+      "swiftlint",
+      "xcbeautify",
+      -- Systemd
+      "systemd-language-server",
+      -- Terraform
+      "terraform-ls",
+      "tflint",
+      -- TOML
+      "taplo",
+      -- TypeScript
+      "biome",
+      "eslint_d",
+      "js-debug-adapter",
+      "prettier",
+      "vtsls",
+      -- YAML
+      "yaml-language-server",
+      -- WebAssembly
+      "wasm-language-tools",
+    },
+  },
+}
