02658dc239d857c0ba388b860aef41e2b4eb13f8
Thomas Saint-Gérand <thomas@devspaceship.com>
2025-12-22 23:46:06 +0100

    [nvim] fix no ft on first load

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.completeopt:append("popup")
+
+-- LSP
+vim.lsp.enable("sourcekit")
+vim.lsp.enable("biome")
+vim.lsp.enable("racket_langserver")
--- dotfiles/nvim/lua/plugins/lspconfig.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "neovim/nvim-lspconfig",
-  opts = function()
-    vim.lsp.enable("sourcekit")
-    vim.lsp.enable("biome")
-    vim.lsp.enable("racket_langserver")
-  end,
-}
