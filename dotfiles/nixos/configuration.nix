06bbd0ea588599933f283a8d15ac8607da2f4469
Thomas Saint-Gérand <thomas@devspaceship.com>
2026-01-04 21:09:51 +0100

    [hyprland] add rofi + dunst

--- dotfiles/nixos/configuration.nix
+++ dotfiles/nixos/configuration.nix
@@ -134,11 +134,14 @@
 
   environment.systemPackages = with pkgs; [
     blueberry
+    dunst
     hyprlauncher
     hyprlock
     hyprpaper
     hyprsysteminfo
+    inotify-tools
     nwg-look
+    rofi
     waybar
     wev
     xdg-desktop-portal-hyprland
@@ -157,12 +160,6 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -170,5 +167,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
