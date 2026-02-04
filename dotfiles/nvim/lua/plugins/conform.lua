bf561d8a4fcf14db6db59c2fc13e205e0060fb20
Thomas Saint-Gérand <thomas@devspaceship.com>
2026-02-04 15:50:38 +0100

    [conform] override timeout

--- dotfiles/nvim/lua/plugins/conform.lua
+++ dotfiles/nvim/lua/plugins/conform.lua
@@ -1,6 +1,12 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    default_format_opts = {
+      timeout_ms = 6000,
+      async = false,
+      quiet = false,
+      lsp_format = "fallback",
+    },
     formatters_by_ft = {
       go = { "goimports", "gofmt" },
       lua = { "stylua" },
