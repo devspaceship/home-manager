abcfe50e8a0fd0ab496e7859c9c7692452cc9fe7
Thomas Saint-Gérand <thomas@devspaceship.com>
2026-02-13 15:25:34 +0100

    [security] remove xargs from opencode whitelist

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -13,3 +13,21 @@ vim.lsp.enable("racket_langserver")
 
 -- VimTex
 vim.g.vimtex_view_method = "zathura"
+-- vim.g.vimtex_compiler_latexmk.aux_dir = "./.aux"
+vim.g.vimtex_compiler_latexmk = {
+  aux_dir = "./.aux",
+}
+-- let g:vimtex_compiler_latexmk = {
+--         \ 'aux_dir' : '',
+--         \ 'out_dir' : '',
+--         \ 'callback' : 1,
+--         \ 'continuous' : 1,
+--         \ 'executable' : 'latexmk',
+--         \ 'hooks' : [],
+--         \ 'options' : [
+--         \   '-verbose',
+--         \   '-file-line-error',
+--         \   '-synctex=1',
+--         \   '-interaction=nonstopmode',
+--         \ ],
+--         \}
