723155436c0bb53a0ef6062cf118ea1fa6b2c35c
Thomas Saint-Gérand <thomas@devspaceship.com>
2026-03-14 00:46:30 +0100

    [zellij] add minimal config
    [zellij] add minimal config

--- dotfiles/nvim/lua/plugins/conform.lua
+++ dotfiles/nvim/lua/plugins/conform.lua
@@ -10,6 +10,8 @@ return {
     formatters_by_ft = {
       go = { "goimports", "gofmt" },
       lua = { "stylua" },
+      kdl = { "kdlfmt" },
+      rust = { "rustfmt" },
       python = function(bufnr)
         if require("conform").get_formatter_info("ruff_format", bufnr).available then
           return { "ruff_format" }
@@ -17,7 +19,6 @@ return {
           return { "isort", "black" }
         end
       end,
-      rust = { "rustfmt" },
       sh = function(_)
         if os.getenv("DEVSPACE") == "work" then
           return { "trunk" }
